b95faeea96e70735973f0757c4b6c3735cbe70be
Meang Inho <149292274+MeangSung@users.noreply.github.com>
2025-02-22 07:46:58 +0900

    Update 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -8,3 +8,15 @@ join USED_GOODS_FILE as f
 on b.board_id = f.board_id
 where b.views = (select max(views) from used_goods_board)
 order by f.file_id desc
+
+SELECT concat(
+    '/home/grep/src',
+    '/',board_id,
+    '/',file_id,file_name,file_ext
+) as FILE_PATH
+from USED_GOODS_FILE
+where board_id = (select board_id
+                 from used_goods_board
+                order by VIEWS desc
+                limit 1)
+order by file_id desc
