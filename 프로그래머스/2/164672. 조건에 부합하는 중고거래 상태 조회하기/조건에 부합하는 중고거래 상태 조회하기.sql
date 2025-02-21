1109e17adebe2f560dcbd2f538e943f2a338fd9b
Meang Inho <149292274+MeangSung@users.noreply.github.com>
2025-02-22 07:59:27 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,10 @@
+SELECT board_id, writer_id, title, price, (
+    case
+        when status = 'SALE' then '판매중'
+        when status = 'DONE' then '거래완료'
+        else '예약중'
+    end
+) as status
+from USED_GOODS_BOARD
+where date_format(created_date, '%Y%m%d') = '20221005'
+order by board_id desc;
