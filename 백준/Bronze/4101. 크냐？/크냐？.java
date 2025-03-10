92d2fdf885bd260363763f4f8440b4c215035166
Meang Inho <149292274+MeangSung@users.noreply.github.com>
2025-03-10 16:21:04 +0900

    [Bronze V] Title: 크냐?, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while(true){
+            st = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            if(n == 0 && m == 0) break;
+
+            stringBuilder.append(n > m ? "Yes" : "No").append("\n");
+        }
+
+        System.out.println(stringBuilder);
+
+    }
+}
