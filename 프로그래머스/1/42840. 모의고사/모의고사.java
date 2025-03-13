4c85ebbb118362b5537adf2ee3880216544eeffe
Meang Inho <149292274+MeangSung@users.noreply.github.com>
2025-03-13 10:48:11 +0900

    [level 1] Title: 모의고사, Time: 5.80 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -3,58 +3,24 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] answers) {
+        int[][] patterns ={
+            {1,2,3,4,5},
+            {2,1,2,3,2,4,2,5},
+            {3,3,1,1,2,2,4,4,5,5}
+        };
         
-        int[] res = new int[3];
-        
-        for(int i = 1; i <= answers.length; i++){
-            
-            //1번 수포자
-            if(i % 5 == 0 && answers[i-1] == 5) res[0]++;
-            else if(i % 5 == answers[i-1]) res[0]++;
-            
-            
-            //2번 수포자
-            if(i%2==1 && answers[i-1] == 2) res[1]++;
-            else{
-                switch(i%8){
-                    case 2:
-                        if(answers[i-1] == 1) res[1]++;
-                        break;
-                    case 4:
-                        if(answers[i-1] == 3) res[1]++;
-                        break;
-                    case 6:
-                        if(answers[i-1] == 4) res[1]++;
-                        break;
-                    case 0:
-                        if(answers[i-1] == 5) res[1]++;
-                        break;
-                }
+        int[] match = new int[3];
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < answers.length; j++){
+                if(patterns[i][j%patterns[i].length] == answers[j]) match[i]++;
             }
-            
-            //3번 수포자
-            switch (i % 10) {
-                case 0,9:
-                    if(answers[i-1] == 5) res[2]++;
-                    break;
-                case 1,2:
-                    if(answers[i-1] == 3) res[2]++;
-                    break;
-                case 3,4:
-                    if(answers[i-1] == 1) res[2]++;
-                    break;
-                case 5,6:
-                    if(answers[i-1] == 2) res[2]++;
-                    break;
-                case 7,8:
-                    if(answers[i-1] == 4) res[2]++;
-                    break;
-            }         
         }
-        int max = Math.max(res[0],Math.max(res[1],res[2]));
         
-        int[] result = IntStream.range(0,res.length).filter(i -> res[i] >= max).map(i -> i+1).toArray();
+        int max = Math.max(match[0],Math.max(match[1],match[2]));
         
-        return result;
+        return IntStream.range(0,3)
+            .filter(i -> match[i] >= max)
+            .map(i -> i+1)
+            .toArray();
     }
 }
