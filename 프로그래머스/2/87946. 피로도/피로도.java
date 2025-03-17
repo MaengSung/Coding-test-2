9e51096b3360bfe10170d56a15685dab0bfbab72
Meang Inho <149292274+MeangSung@users.noreply.github.com>
2025-03-17 18:00:43 +0900

    [level 2] Title: 피로도, Time: 3.31 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -3,12 +3,12 @@ class Solution {
     public int solution(int k, int[][] dungeons) {
         boolean[] visited = new boolean[dungeons.length];
         
-        recur(0,0,k,dungeons,visited);
+        recur(0,k,dungeons,visited);
         
         return max;
     }
     
-    private void recur(int r, int cnt, int k, int[][] dungeons, boolean[] visited){
+    private void recur(int cnt, int k, int[][] dungeons, boolean[] visited){
         max = Math.max(max, cnt);
         
         for(int i = 0 ; i < dungeons.length; i++){
@@ -16,7 +16,7 @@ class Solution {
             if(k < dungeons[i][0]) continue;
             
             visited[i] = true;
-            recur(r+1,cnt+1,k - dungeons[i][1], dungeons, visited);
+            recur(cnt+1,k - dungeons[i][1], dungeons, visited);
             visited[i] = false;
         }
     }
