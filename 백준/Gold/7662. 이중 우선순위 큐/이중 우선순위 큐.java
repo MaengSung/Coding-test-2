79765f4ed682656f165e255e38877795b7d386ef
Meang Inho <149292274+MeangSung@users.noreply.github.com>
2025-04-10 12:21:55 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2604 ms, Memory: 359576 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,72 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main{
-    private static Map<Integer,Integer> map;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+
         int t = Integer.parseInt(br.readLine());
+
         for(int test = 0; test < t; test++) {
-
-            map = new HashMap<>();
-
-            Queue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
-            Queue<Integer> min = new PriorityQueue<>();
-
             int n = Integer.parseInt(br.readLine());
+
+            TreeMap<Integer, Integer> map = new TreeMap<>();
+
             for(int i = 0; i < n; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                String op = st.nextToken();
 
-                if(op.equals("I")) {
+                String op = st.nextToken();
+                if(op.equals("I")){
                     int num = Integer.parseInt(st.nextToken());
-                    max.add(num);
-                    min.add(num);
-                    map.put(num,map.getOrDefault(num,0)+1);
+                    map.put(num, map.getOrDefault(num, 0) + 1);
                 }
                 else{
-                    int type = Integer.parseInt(st.nextToken());
-
                     if(map.isEmpty()) continue;
-
-                    if(type == 1) delete(max);
-                    if(type == -1) delete(min);
+                    int type = Integer.parseInt(st.nextToken());
+                    int removedNum;
+                    if(type == 1){
+                        removedNum = map.lastKey();
+                    }
+                    else{
+                        removedNum = map.firstKey();
+                    }
+                    if(map.put(removedNum,map.get(removedNum)-1) == 1){
+                        map.remove(removedNum);
+                    }
                 }
-
             }
-            if(map.isEmpty()) sb.append("EMPTY").append("\n");
-            else{
-                int res = delete(max);
-                sb.append(res).append(" ");
-                if(!map.isEmpty()) res = delete(min);
-                sb.append(res).append("\n");
-
-            }
+            if(map.isEmpty()) sb.append("EMPTY\n");
+            else sb.append(map.lastKey()).append(" ").append(map.firstKey()).append("\n");
         }
+
         System.out.println(sb);
-    }
-
-    private static int delete(Queue<Integer> q) {
-        int res = 0;
-        while(true){
-            res = q.poll();
-
-            int cnt = map.getOrDefault(res,0);
-            if(cnt == 0) continue;
-
-            if(cnt == 1) map.remove(res);
-            if(cnt > 1) map.put(res,cnt - 1);
-            break;
-        }
-        return res;
     }
 }
