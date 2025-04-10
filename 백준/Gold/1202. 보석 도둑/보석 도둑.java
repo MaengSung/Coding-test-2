a3bc6a7b4b44417935701358d72d45101b49a57a
Meang Inho <149292274+MeangSung@users.noreply.github.com>
2025-04-10 18:43:13 +0900

    [Gold II] Title: 보석 도둑, Time: 1644 ms, Memory: 115288 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -1,58 +1,55 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
-import java.util.TreeMap;
+import java.io.*;
+import java.util.*;
 
 public class Main{
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        long answer = 0;
+        long ans = 0;
+        
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-
-
+        
         Jewel[] jewels = new Jewel[n];
         int[] bags = new int[k];
-
-        for (int i = 0; i < n; i++) {
+        
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
-            jewels[i] = new Jewel(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            
+            int m = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            
+            jewels[i] = new Jewel(m,v);
         }
         Arrays.sort(jewels);
-
+        
         for(int i = 0; i < k; i++) bags[i] = Integer.parseInt(br.readLine());
         Arrays.sort(bags);
-
+        
         Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         for(int i = 0, j = 0; i < k; i++){
-            while(j < n && (jewels[j].weight <= bags[i])){
+            while(j < n && bags[i] >= jewels[j].weight){
                 pq.offer(jewels[j++].value);
             }
-            if(!pq.isEmpty()) answer += pq.poll();
+            
+            if(!pq.isEmpty()) ans += pq.poll();
         }
-        System.out.println(answer);
+        
+        System.out.println(ans);
     }
-
-    private static class Jewel implements Comparable<Jewel>{
+    
+    static class Jewel implements Comparable<Jewel> {
         int weight;
         int value;
-
+        
         Jewel(int weight, int value){
             this.weight = weight;
             this.value = value;
         }
-
-
+        
         @Override
-        public int compareTo(Jewel o) {
-            if(o.weight == this.weight) return this.value - o.value;
+        public int compareTo(Jewel o){
+            if(this.weight == o.weight) return o.value - this.value;
             return this.weight - o.weight;
         }
     }
