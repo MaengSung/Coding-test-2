d3e8035aabb7aef246cd4be03dbfe8e9a8e512a8
Meang Inho <149292274+MeangSung@users.noreply.github.com>
2025-04-13 16:19:16 +0900

    [Gold II] Title: 문제 추천 시스템 Version 2, Time: 704 ms, Memory: 73176 KB -BaekjoonHub

--- 백준/Gold/21944. 문제 추천 시스템 Version 2/문제 추천 시스템 Version 2.java
+++ 백준/Gold/21944. 문제 추천 시스템 Version 2/문제 추천 시스템 Version 2.java
@@ -1,152 +1,138 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-    static class Problem {
+public class Main{
+    static class Problem{
         int level;
         int group;
 
-        Problem(int level, int group) {
+        Problem(int level, int group){
             this.level = level;
             this.group = group;
         }
     }
 
-    static Map<Integer, Problem> probLevel = new HashMap<>();
-    static TreeMap<Integer, TreeSet<Integer>> probByL = new TreeMap<>();
-    static Map<Integer, TreeMap<Integer, TreeSet<Integer>>> probByGL = new HashMap<>();
+    private static Map<Integer, Problem> prob = new HashMap<>();
+    private static TreeMap<Integer, TreeSet<Integer>> probByL = new TreeMap<>();
+    private static Map<Integer, TreeMap<Integer, TreeSet<Integer>>> probByGL = new HashMap<>();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
-
-        // 초기 문제 등록
-        for (int i = 0; i < N; i++) {
+        for(int i = 0; i < N; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
+
             int P = Integer.parseInt(st.nextToken());
             int L = Integer.parseInt(st.nextToken());
             int G = Integer.parseInt(st.nextToken());
 
-            addProblem(P, L, G);
+            addProblem(P,L,G);
         }
 
-        int M = Integer.parseInt(br.readLine());
-        while (M-- > 0) {
+        int Q = Integer.parseInt(br.readLine());
+        while(Q-- > 0){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            String cmd = st.nextToken();
+            String op = st.nextToken();
 
-            if (cmd.equals("recommend")) {
-                int G = Integer.parseInt(st.nextToken());
+            if(op.equals("recommend")){
+                int g = Integer.parseInt(st.nextToken());
                 int x = Integer.parseInt(st.nextToken());
 
-                TreeMap<Integer, TreeSet<Integer>> levelMap = probByGL.get(G);
-                if (levelMap == null) {
-                    sb.append("-1\n");
-                    continue;
-                }
+                TreeMap<Integer,TreeSet<Integer>> levelMap = probByGL.get(g);
 
-                if (x == 1) {
-                    for (int level : levelMap.descendingKeySet()) {
-                        TreeSet<Integer> probs = levelMap.get(level);
-                        if (!probs.isEmpty()) {
-                            sb.append(probs.last()).append("\n");
-                            break;
-                        }
-                    }
-                } else {
-                    for (int level : levelMap.keySet()) {
-                        TreeSet<Integer> probs = levelMap.get(level);
-                        if (!probs.isEmpty()) {
-                            sb.append(probs.first()).append("\n");
-                            break;
-                        }
-                    }
+                if(x == 1) {
+                    TreeSet<Integer> problems = levelMap.get(levelMap.lastKey());
+                    int problem = problems.last();
+                    sb.append(problem).append("\n");
+                }
+                else if(x == -1) {
+                    TreeSet<Integer> problems = levelMap.get(levelMap.firstKey());
+                    int problem = problems.first();
+                    sb.append(problem).append("\n");
                 }
             }
 
-            else if (cmd.equals("recommend2")) {
+            else if(op.equals("recommend2")){
                 int x = Integer.parseInt(st.nextToken());
-                if (x == 1) {
-                    for (int level : probByL.descendingKeySet()) {
-                        TreeSet<Integer> probs = probByL.get(level);
-                        if (!probs.isEmpty()) {
-                            sb.append(probs.last()).append("\n");
-                            break;
-                        }
-                    }
-                } else {
-                    for (int level : probByL.keySet()) {
-                        TreeSet<Integer> probs = probByL.get(level);
-                        if (!probs.isEmpty()) {
-                            sb.append(probs.first()).append("\n");
-                            break;
-                        }
-                    }
+
+                if(x == 1) {
+                    TreeSet<Integer> problems = probByL.get(probByL.lastKey());
+                    int problem = problems.last();
+                    sb.append(problem).append("\n");
+                }
+                if(x == -1) {
+                    TreeSet<Integer> problems = probByL.get(probByL.firstKey());
+                    int problem = problems.first();
+                    sb.append(problem).append("\n");
                 }
             }
 
-            else if (cmd.equals("recommend3")) {
+            else if(op.equals("recommend3")){
                 int x = Integer.parseInt(st.nextToken());
-                int L = Integer.parseInt(st.nextToken());
+                int l = Integer.parseInt(st.nextToken());
 
-                if (x == 1) {
-                    Integer level = probByL.ceilingKey(L);
-                    if (level == null) {
-                        sb.append("-1\n");
-                    } else {
-                        TreeSet<Integer> probs = probByL.get(level);
-                        sb.append(probs.first()).append("\n");
+                if(x == 1){
+                    Integer level = probByL.ceilingKey(l);
+                    if(level == null) {
+                        sb.append("-1").append("\n");
                     }
-                } else {
-                    Integer level = probByL.floorKey(L - 1);
-                    if (level == null) {
-                        sb.append("-1\n");
-                    } else {
-                        TreeSet<Integer> probs = probByL.get(level);
-                        sb.append(probs.last()).append("\n");
+                    else{
+                        int problem = probByL.get(level).first();
+                        sb.append(problem).append("\n");
+                    }
+                }
+                if(x == -1) {
+                    Integer level = probByL.floorKey(l-1);
+                    if(level == null) {
+                        sb.append("-1").append("\n");
+                    }
+                    else{
+                        int problem = probByL.get(level).last();
+                        sb.append(problem).append("\n");
                     }
                 }
             }
 
-            else if (cmd.equals("add")) {
-                int P = Integer.parseInt(st.nextToken());
-                int L = Integer.parseInt(st.nextToken());
-                int G = Integer.parseInt(st.nextToken());
-                addProblem(P, L, G);
+            else if(op.equals("add")){
+                int p = Integer.parseInt(st.nextToken());
+                int l = Integer.parseInt(st.nextToken());
+                int g = Integer.parseInt(st.nextToken());
+                addProblem(p,l,g);
             }
 
-            else if (cmd.equals("solved")) {
-                int P = Integer.parseInt(st.nextToken());
-                removeProblem(P);
+            else if(op.equals("solved")){
+                int p = Integer.parseInt(st.nextToken());
+                solveProblem(p);
             }
         }
 
-        System.out.print(sb);
+        System.out.println(sb);
     }
 
-    static void addProblem(int P, int L, int G) {
-        probLevel.put(P, new Problem(L, G));
+    private static void solveProblem(int p){
+        Problem problem = prob.get(p);
+
+        int l = problem.level;
+        int g = problem.group;
+
+        probByL.get(l).remove(p);
+        if(probByL.get(l).isEmpty()) probByL.remove(l);
+
+        probByGL.get(g).get(l).remove(p);
+        if(probByGL.get(g).get(l).isEmpty()) probByGL.get(g).remove(l);
+        if(probByGL.get(g).isEmpty()) probByGL.remove(g);
+
+        prob.remove(p);
+    }
+
+    private static void addProblem(int P, int L, int G){
+        prob.put(P,new Problem(L,G));
 
         probByL.computeIfAbsent(L, k -> new TreeSet<>()).add(P);
 
         probByGL.computeIfAbsent(G, k -> new TreeMap<>());
         probByGL.get(G).computeIfAbsent(L, k -> new TreeSet<>()).add(P);
-    }
-
-    static void removeProblem(int P) {
-        Problem prob = probLevel.get(P);
-        int L = prob.level;
-        int G = prob.group;
-
-        probByL.get(L).remove(P);
-        if (probByL.get(L).isEmpty()) probByL.remove(L);
-
-        probByGL.get(G).get(L).remove(P);
-        if (probByGL.get(G).get(L).isEmpty()) probByGL.get(G).remove(L);
-        if (probByGL.get(G).isEmpty()) probByGL.remove(G);
-
-        probLevel.remove(P);
     }
 }
