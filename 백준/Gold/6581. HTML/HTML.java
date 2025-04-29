a9500a01a32f8f2783d254c983ec5b4a8a19d998
Meang Inho <149292274+MeangSung@users.noreply.github.com>
2025-04-29 16:33:15 +0900

    [Gold V] Title: HTML, Time: 100 ms, Memory: 14012 KB -BaekjoonHub

--- 백준/Gold/6581. HTML/HTML.java
+++ 백준/Gold/6581. HTML/HTML.java
@@ -1,26 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    static StringBuilder res = new StringBuilder();
-    static int cnt = 0;
-    public static void main(String[] args) throws IOException {
+public class Main{
+    private static StringBuilder res = new StringBuilder();
+    private static int cnt = 0;
+    public static void main(String[] args) throws IOException{
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        String line = "";
+        
         List<String> tokens = new ArrayList<>();
-        while ((line = bufferedReader.readLine()) != null) {
+        String line;
+        while((line = bufferedReader.readLine()) != null){
             StringTokenizer st = new StringTokenizer(line," \t\n");
-            while (st.hasMoreTokens()) {
+            while(st.hasMoreTokens()){
                 tokens.add(st.nextToken());
             }
         }
-
-        for(String token : tokens) {
+        
+        for(String token : tokens){
             if(token.equals("<hr>")){
                 hr();
                 continue;
@@ -29,26 +25,26 @@ public class Main {
                 br();
                 continue;
             }
-            if(token.length() + cnt + (cnt > 0 ? 1 : 0) > 80){
+            if(cnt+token.length()+(cnt>0?1:0) > 80){
                 br();
             }
+            
             if(cnt > 0) res.append(" ");
             res.append(token);
-            cnt += token.length() + (cnt > 0 ? 1 : 0);
+            cnt += token.length() + (cnt>0?1:0);
         }
         br();
-
         System.out.println(res);
     }
-
+    
+    private static void br(){
+        res.append("\n");
+        cnt= 0;
+    }
+    
     private static void hr(){
         if(cnt > 0) br();
         res.append("--------------------------------------------------------------------------------").append("\n");
-        cnt = 0;
-    }
-
-    private static void br(){
-        res.append("\n");
         cnt = 0;
     }
 }
