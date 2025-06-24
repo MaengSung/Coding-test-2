f1c25f37f0dcf51077c1c3da6a0621017936d82c
Meang Inho <149292274+MeangSung@users.noreply.github.com>
2025-06-24 20:18:14 +0900

    [Gold I] Title: 2048 (Easy), Time: 156 ms, Memory: 20116 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -1,140 +1,158 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    
-    static int n, answer, map[][];
-    
-    public static void main(String[] args) throws Exception {
+    static int[][] board;
+    static int n;
+    static int max = 0;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         n = Integer.parseInt(br.readLine());
-        answer = 0;
-        map = new int[n][n];
-        StringTokenizer stz;
-        for(int i = 0; i < n; i++) {
-            stz = new StringTokenizer(br.readLine());
-            for(int j = 0; j < n; j++)
-                map[i][j] = Integer.parseInt(stz.nextToken());
+        board = new int[n][n];
+
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
-        
+
         game(0);
-        System.out.println(answer);
+        System.out.println(max);
     }
-    
-    public static void game(int count) {
-        if(count == 5) {
+
+    private static void game(int count) {
+        if(count == 5){
             findMax();
             return;
         }
-        int copy[][] = new int[n][n];
-        for(int i = 0; i < n; i++)
-            copy[i] = map[i].clone();
-        
-        for(int i = 0; i < 4; i++) {
+
+        int[][] copy = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            copy[i] = board[i].clone();
+        }
+
+        for(int i = 0; i < 4; i++){
             move(i);
-            game(count+1);
-            for(int a = 0; a < n; a++)
-                map[a] = copy[a].clone();
+            game(count + 1);
+            for(int j = 0; j < n; j++){
+                board[j] = copy[j].clone();
+            }
         }
     }
-    
-    public static void move(int dir) {
-        switch(dir) {
-            //위로 몰기
+
+    private static void move(int dir) {
+        switch (dir) {
             case 0:
-                for(int i = 0; i < n; i++) {
-                    int index = 0;
-                    int block = 0;
-                    for(int j = 0; j < n; j++) {
-                        if(map[j][i] != 0) {
-                            if(block == map[j][i]) {
-                                map[index - 1][i] = block * 2;
-                                block = 0;
-                                map[j][i] = 0;
-                            }
-                            else {
-                                block = map[j][i];
-                                map[j][i] = 0;
-                                map[index][i] = block;
-                                index++;
-                            }
-                        }
-                    }
-                }
+                up();
                 break;
-            //왼쪽으로 몰기
             case 1:
-                for(int i = 0; i < n; i++) {
-                    int index = n - 1;
-                    int block = 0;
-                    for(int j = n - 1; j >= 0; j--) {
-                        if(map[j][i] != 0) {
-                            if(block == map[j][i]) {
-                                map[index + 1][i] = block * 2;
-                                block = 0;
-                                map[j][i] = 0;
-                            }
-                            else {
-                                block = map[j][i];
-                                map[j][i] = 0;
-                                map[index][i] = block;
-                                index--;
-                            }
-                        }
-                    }
-                }
+                down();
                 break;
-            //왼쪽으로 몰기
             case 2:
-                for(int i = 0; i < n; i++) {
-                    int index = 0;
-                    int block = 0;
-                    for(int j = 0; j < n; j++) {
-                        if(map[i][j] != 0) {
-                            if(block == map[i][j]) {
-                                map[i][index - 1] = block * 2;
-                                block = 0;
-                                map[i][j] = 0;
-                            }
-                            else {
-                                block = map[i][j];
-                                map[i][j] = 0;
-                                map[i][index] = block;
-                                index++;
-                            }
-                        }
-                    }
-                }
+                left();
                 break;
-            //오른쪽으로 몰기
             case 3:
-                for(int i = 0; i < n; i++) {
-                    int index = n - 1;
-                    int block = 0;
-                    for(int j = n - 1; j >= 0; j--) {
-                        if(map[i][j] != 0) {
-                            if(block == map[i][j]) {
-                                map[i][index + 1] = block * 2;
-                                block = 0;
-                                map[i][j] = 0;
-                            }
-                            else {
-                                block = map[i][j];
-                                map[i][j] = 0;
-                                map[i][index] = block;
-                                index--;
-                            }
-                        }
-                    }
-                }
+                right();
                 break;
         }
     }
-    
-    public static void findMax() {
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < n; j++)
-                answer = Math.max(answer, map[i][j]);
+
+    private static void findMax() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                max = Math.max(board[i][j], max);
+            }
+        }
+    }
+
+    static void right() {
+        for (int i = 0; i < n; i++) {
+            int index = n-1;
+            int block = 0;
+            for(int j = n-1; j >= 0; j--) {
+                if(board[i][j] != 0){
+                    if(board[i][j] == block){
+                        board[i][index + 1] = block * 2;
+                        board[i][j] = 0;
+                        block = 0;
+                    }
+                    else{
+                        block = board[i][j];
+                        board[i][j] = 0;
+                        board[i][index] = block;
+                        index--;
+                    }
+                }
+            }
+        }
+
+    }
+    static void left() {
+        for (int i = 0; i < n; i++) {
+            int index = 0;
+            int block = 0;
+            for(int j = 0; j < n; j++) {
+                if(board[i][j] != 0){
+                    if(board[i][j] == block){
+                        board[i][index - 1] = block * 2;
+                        board[i][j] = 0;
+                        block = 0;
+                    }
+                    else{
+                        block = board[i][j];
+                        board[i][j] = 0;
+                        board[i][index] = block;
+                        index++;
+                    }
+                }
+            }
+        }
+    }
+    static void down() {
+        for (int i = 0; i < n; i++) {
+            int index = n-1;
+            int block = 0;
+            for (int j = n-1; j >= 0; j--) {
+                if(board[j][i] != 0){
+                    if(board[j][i] == block){
+                        board[index + 1][i] = block * 2;
+                        board[j][i] = 0;
+                        block = 0;
+                    }
+                    else{
+                        block = board[j][i];
+                        board[j][i] = 0;
+                        board[index][i] = block;
+                        index--;
+                    }
+                }
+            }
+        }
+    }
+    static void up() {
+        for (int i = 0; i < n; i++) {
+            int index = 0;
+            int block = 0;
+            for (int j = 0; j < n; j++) {
+                if(board[j][i] != 0){
+                    if(board[j][i] == block){
+                        board[index - 1][i] = block * 2;
+                        board[j][i] = 0;
+                        block = 0;
+                    }
+                    else{
+                        block = board[j][i];
+                        board[j][i] = 0;
+                        board[index][i] = block;
+                        index++;
+                    }
+                }
+            }
+        }
     }
 }
