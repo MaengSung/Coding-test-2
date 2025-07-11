90222bac762ea916264af32a96b2d79af798ff49
Meang Inho <149292274+MeangSung@users.noreply.github.com>
2025-07-11 09:34:14 +0900

    [Silver I] Title: 회전 초밥, Time: 188 ms, Memory: 18700 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.java
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -12,39 +12,29 @@ public class Main {
         int c = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        int[] count = new int[d+1];
+
+        for(int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        int max = 0;
-        int left = 0, right = 0;
-        List<Integer> list = new ArrayList<>();
-        while(left < n){
-            if(n - left >= k){
-                list.add(arr[right]);
-                if(right - left + 1 == k){
-                    Set<Integer> set = new HashSet<>(list);
-                    int size = set.size();
-                    if(!set.contains(c)) size++;
-                    max= Math.max(max, size);
-                    list.remove(0);
-                    left++;
-                }
-                right++;
-            }else {
-                right = k - n + left - 1;
-                list.add(arr[right]);
-                if(n - left + right + 1 == k){
-                    Set<Integer> set = new HashSet<>(list);
-                    int size = set.size();
-                    if(!set.contains(c)) size++;
-                    max= Math.max(max, size);
-                    list.remove(0);
-                    left++;
-                }
-            }
-            if(max == k + 1) break;
+        int kind = 0;
+        for(int i = 0; i < k; i++){
+            if(count[arr[i]]++ == 0)
+                kind++;
         }
+
+        int max = kind;
+        if(count[c] == 0) max++;
+        for(int i = 1; i < n; i++){
+            if(--count[arr[i-1]] == 0) kind--;
+            if(count[arr[(i + k -1) % n]]++ == 0) kind++;
+
+            int temp = kind;
+            if(count[c] == 0) temp++;
+            max = Math.max(max, temp);
+        }
+
         System.out.println(max);
     }
 }
