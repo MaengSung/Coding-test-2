fdcdb6e1c997d8efdbb7b551f9dfbc49937bb760
Meang Inho <149292274+MeangSung@users.noreply.github.com>
2025-08-08 20:48:36 +0900

    [Gold IV] Title: 합이 0, Time: 584 ms, Memory: 30156 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.java
+++ 백준/Gold/3151. 합이 0/합이 0.java
@@ -1,52 +1,46 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
         Arrays.sort(arr);
 
         long cnt = 0;
-        for (int i = 0; i < n - 2; i++) {
+        for(int i = 0; i < n-2; i++){
             int left = i + 1;
             int right = n - 1;
-
-            while (left < right) {
+            while(left < right){
                 int sum = arr[i] + arr[left] + arr[right];
-
-                if (sum == 0) {
-                    if (arr[left] == arr[right]) { 
-                        // left~right 사이의 모든 조합 수
+                if(sum == 0){
+                    if(arr[left] == arr[right]){
                         int len = right - left + 1;
                         cnt += (long) len * (len - 1) / 2;
-                        break; 
+                        break;
                     }
 
-                    // 같은 값이 몇 개 있는지 세기
-                    int lCount = 1, rCount = 1;
-                    while (left + 1 < right && arr[left] == arr[left + 1]) {
-                        lCount++;
+                    int lCnt = 1, rCnt = 1;
+                    while(left < right - 1 && arr[left] == arr[left+1]){
+                        lCnt++;
                         left++;
                     }
-                    while (right - 1 > left && arr[right] == arr[right - 1]) {
-                        rCount++;
+                    while(right > left + 1 && arr[right] == arr[right - 1]){
+                        rCnt++;
                         right--;
                     }
 
-                    cnt += (long) lCount * rCount;
+                    cnt += (long)lCnt * rCnt;
                     left++;
                     right--;
                 }
-                else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
-                }
+
+                else if(sum < 0) left++;
+                else right--;
             }
         }
 
