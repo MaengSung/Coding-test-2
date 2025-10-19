b9950318777fcf855a339c5e3a40cc4135143168
Meang Inho <149292274+MaengSung@users.noreply.github.com>
2025-10-19 15:17:20 +0900

    [Gold I] Title: 열쇠, Time: 184 ms, Memory: 22016 KB -BaekjoonHub

--- 백준/Gold/9328. 열쇠/열쇠.java
+++ 백준/Gold/9328. 열쇠/열쇠.java
@@ -1,113 +1,90 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main{
-    private static int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-    private static int n, m;
-    private static boolean[][] visited;
-    private static char[][] map;
-    private static Queue<int[]> moves;
-    private static List<int[]>[] doors;
-    private static boolean[] keys;
-
-    public static void main(String[] args) throws IOException {
+    private static final int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        while(t-->0){
+
+        while(t-- > 0){
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             int h = Integer.parseInt(st.nextToken());
             int w = Integer.parseInt(st.nextToken());
 
-            n = h + 2;
-            m = w + 2;
+            int n = h + 2;
+            int m = w + 2;
+            int cnt = 0;
 
-            map = new char[n][m];
+            char[][] map = new char[n][m];
+            boolean[][] visited = new boolean[n][m];
+            boolean[] keys = new boolean[26];
+            List<int[]>[] blockedByDoors = new List[26];
+            Queue<int[]> move = new LinkedList<>();
 
-            for(int i = 0; i < n; i++) Arrays.fill(map[i], '.');
-            for (int i = 0; i < h; i++) {
+            for(int i = 0; i < n; i++) Arrays.fill(map[i],'.');
+            for(int i = 0; i < 26; i++) blockedByDoors[i] = new ArrayList<>();
+
+            for(int i = 0; i < h; i++){
                 String line = br.readLine();
-                for (int j = 0; j < w; j++) {
-                    map[i + 1][j + 1] = line.charAt(j);
+                for(int j = 0; j < w; j++){
+                    map[i+1][j+1] = line.charAt(j);
                 }
             }
 
-            doors = new List[26];
-            for(int i = 0; i < 26; i++)doors[i] = new ArrayList<>();
-            keys = new boolean[26];
-            visited = new boolean[n][m];
+            String keysToString = br.readLine();
+            if(!keysToString.equals("0"))
+                for(char c : keysToString.toCharArray()) keys[c - 'a'] = true;
 
-
-            String inputKeys = br.readLine();
-            if(!inputKeys.equals("0")){
-                for(char key : inputKeys.toCharArray()){
-                    keys[key - 'a'] = true;
-                }
-            }
-
-            moves = new LinkedList<>();
-            moves.add(new int[]{0,0});
+            move.add(new int[]{0,0});
             visited[0][0] = true;
+            while(!move.isEmpty()){
+                int[] cur = move.poll();
+                int x = cur[0], y = cur[1];
 
-            int cnt = bfsFind();
+                for(int[] dir : dirs){
+                    int cx = x + dir[0], cy = y + dir[1];
 
+                    if(cx < 0 || cx > n-1 || cy < 0 || cy > m-1) continue;
+                    if(visited[cx][cy]) continue;
+                    if(map[cx][cy] == '*') continue;
+
+                    char loc = map[cx][cy];
+                    visited[cx][cy] = true;
+                    if(loc == '.'){
+                        move.add(new int[]{cx,cy});
+                    }
+                    else if(loc == '$'){
+                        move.add(new int[]{cx,cy});
+                        cnt++;
+                    }
+                    else if(Character.isUpperCase(loc)){
+                        int pos = loc - 'A';
+                        if(keys[pos]){
+                            move.add(new int[]{cx,cy});
+                        }
+                        else{
+                            blockedByDoors[pos].add(new int[]{cx,cy});
+                        }
+                    }
+                    else if(Character.isLowerCase(loc)){
+                        int pos = loc - 'a';
+                        keys[pos] = true;
+                        move.add(new int[]{cx,cy});
+
+                        for(int[] door: blockedByDoors[pos]){
+                            move.add(new int[]{door[0],door[1]});
+                        }
+
+                        blockedByDoors[pos].clear();
+                    }
+                }
+            }
             sb.append(cnt).append("\n");
         }
         System.out.println(sb.toString());
-    }
-
-    private static int bfsFind(){
-        int cnt = 0;
-        while(!moves.isEmpty()){
-            int[] move = moves.poll();
-            int x = move[0], y = move[1];
-            for(int[] dir: dirs){
-                int cx = x + dir[0], cy = y + dir[1];
-
-                if(cx < 0 || cx >= n || cy < 0 || cy >= m) continue;
-                if(visited[cx][cy]) continue;
-                if(map[cx][cy] == '*') continue;
-
-                char pos = map[cx][cy];
-                visited[cx][cy] = true;
-
-                if(pos == '.'){
-                    moves.add(new int[]{cx,cy});
-                }
-                else if(pos == '$'){
-                    moves.add(new int[]{cx,cy});
-                    cnt++;
-                }
-                else if(Character.isAlphabetic(pos)){
-                    if(Character.isUpperCase(pos)){
-                        if(keys[pos - 'A']) {
-                            moves.add(new int[]{cx,cy});
-                        }
-                        else{
-                            doors[pos-'A'].add(new int[]{cx,cy});
-                        }
-                    }
-                    else if(Character.isLowerCase(pos)){
-                        keys[pos - 'a'] = true;
-                        moves.add(new int[]{cx, cy});
-
-                        for (int[] door : doors[pos - 'a']) {
-                            moves.add(new int[]{door[0], door[1]});
-                        }
-                        doors[pos - 'a'].clear();
-                    }
-                }
-            }
-        }
-        return cnt;
     }
 }
